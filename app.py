30c08dece32094ab0a01b1550df9be3bb87b63db
thatflippingfool <thatflippingfool@gmail.com>
2018-08-30 15:08:50 -0400

    Combine /echo and /echoer

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import Flask and templates
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 
 # Create Flask object
 app = Flask(__name__)
@@ -10,30 +10,30 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
 
-	# Generate html
-	return render_template('index.html', message='Hello World!')
+    # Generate html
+    return render_template('index.html', message='Hello World!')
+
 
 # Print out some of the contents of the uploaded file
-@app.route('/echo')
+@app.route('/echo', methods=['POST', 'GET'])
 def echo():
-	return render_template('echo.html', echo='')
 
-# API handler for '/echo'
-@app.route('/echoer', methods = ['POST'])
-def echoer():
-	
-	# Check that it is a POST request
-	if request.method == 'POST':
-		
-		# Confirm a file is uploaded
-		if 'file' not in request.files:
-			return render_template('echo.html', echo='No file uploaded!')
-		
-		# Read out some of the file contents
-		file = [line.decode() for line in request.files['file'].readlines(1000)]
-		return render_template('echo.html', echo=''.join(file))
+    # Check that it is a POST request
+    if request.method == 'POST':
+
+        # Confirm a file is uploaded
+        if 'file' not in request.files:
+            return render_template('echo.html', echo='No file uploaded!')
+
+        # Read out some of the file contents
+        file = [line.decode() for line in request.files['file'].readlines(1000)]
+        return render_template('echo.html', echo=''.join(file))
+
+    # GET request
+    return render_template('echo.html', echo='')
+
 
 # Enable python script to be executable
 if __name__ == "__main__":
-	app.secret_key = 'super secret key'
-	app.run()
+    app.secret_key = 'super secret key'
+    app.run()
